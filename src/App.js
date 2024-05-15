9c8a04f2c5d1bf54d3eace446b75b12cb579dd25
633040156-4 <treepaech@kkumail.com>
2024-05-15 14:56:58 +0700

    chang name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Treepaech Treechan
         </a>
       </header>
     </div>
