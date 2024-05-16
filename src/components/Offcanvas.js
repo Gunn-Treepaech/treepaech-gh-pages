6adbb1e1e6f0e5400416f74321ad642f59c58ed8
633040156-4 <treepaech@kkumail.com>
2024-05-16 14:47:50 +0700

    add send mail

--- src/components/Offcanvas.js
+++ src/components/Offcanvas.js
@@ -1,9 +1,35 @@
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-import "./Offcanvas.css"
+import Button from "react-bootstrap/Button";
+import "./Offcanvas.css";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 export default function Offcanva({ show, handleClose }) {
+  const formRef = useRef(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_bqspy4j",
+        "template_gcufh59",
+        formRef.current,
+        "Yku_pMm7R5lUl5VNe"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (formRef.current) {
+            formRef.current.reset();
+          }
+        },
+        (error) => {
+          console.error(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <Offcanvas show={show} onHide={handleClose}>
@@ -11,19 +37,25 @@ export default function Offcanva({ show, handleClose }) {
           <Offcanvas.Title>Contact Me</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <Form action="https://formsubmit.co/g.treepaech@gmail.com" method="POST">
+          <Form ref={formRef} onSubmit={sendEmail}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Email: </Form.Label>
-              <Form.Control type="email" name="email" placeholder="name@example.com" />
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="name@example.com"
+              />
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
               <Form.Label>Message: </Form.Label>
-              <Form.Control type="text" name="name" as="textarea" rows={3} />
-              <Button className="sendButton" variant="primary" type="submit">Send</Button>{' '}
+              <Form.Control type="text" name="message" as="textarea" rows={3} />
             </Form.Group>
+            <Button className="sendButton" variant="primary" type="submit">
+              Send
+            </Button>
           </Form>
         </Offcanvas.Body>
       </Offcanvas>
