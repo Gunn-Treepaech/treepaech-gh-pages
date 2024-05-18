50a31574575485886bf431d0a9d953ab3860e2f0
633040156-4 <treepaech@kkumail.com>
2024-05-18 13:27:03 +0700

    fix bug

--- src/components/Navbars.js
+++ src/components/Navbars.js
@@ -3,8 +3,8 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanva from "./Offcanvas";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Navbars.css"
+// import { Link } from "react-router-dom";
+import "./Navbars.css";
 
 export default function Navbar_component() {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
@@ -20,17 +20,11 @@ export default function Navbar_component() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link>
-                <Link to="/" className="navbarItem">
-                  Home
-                </Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About Me</Nav.Link>
+              <Nav.Link className="navbarItem" onClick={handleShowOffcanvas}>
+                Contact Me
               </Nav.Link>
-              <Nav.Link>
-                <Link to="/about" className="navbarItem">
-                About Me
-                </Link>
-              </Nav.Link>
-              <Nav.Link className="navbarItem" onClick={handleShowOffcanvas}>Contact Me</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import AboutMe from './pages/AboutMe';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "about",
-    element: <AboutMe />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
